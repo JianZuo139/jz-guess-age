2f703d44c525f65f5eb932ff60c56ea4a6899cce
JianZuo139 <jianzuoteh23@gmail.com>
2021-12-19 04:16:25 +0000

    finally

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    let name = ''
+    let age = ''
+    async function getAge() {
+    fetch('https://api.agify.io/?name=John'+name )
+    .then((response) => response.json())
+    .then((data) => {
+  	age = data.age
+        });
+    }
+</script>
+
+<h1>Guess Age</h1>
+
+<form on:submit|preventDefault={getAge}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+
+<p>Age:{age}</p>
